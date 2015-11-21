1e0fd5934135a0f2eb5554eb4fc35393af7c7a7d
Anton Aleksandrov <antona@us.ibm.com>
2015-11-21 08:58:16 -0600

    updated to java boilerplate

---
+++ buildwar.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+
+set -x
+
+app_name=bms-hellotodo-strongloop
+app_git_url=https://github.com/ibm-bluemix-mobile-services/${app_name}.git
+
+# Clean up /target
+rm -rf target
+mkdir target
+
+cd boilerplate/imf
+
+# Clean up before cloning a repo
+rm -rf ${app_name}
+rm -rf .git
+
+# Clone a repo
+git clone ${app_git_url}
+cd ${app_name}
+rm -rf .git
+cd .. #back to /boilerplate/imf
+
+# Create a new repo in /boilerplate/imf
+git init
+git add .
+git commit -m "Created git repo for the boilerplate"
+git gc --aggressive
+git update-server-info
+cd .. #back to /boilerplate
+
+# Jar /boilerplate/imf to boilerplate.war
+jar -cf ../target/boilerplate.war * .cfignore
+
+# Clean up cloned repo and .git folders in /boilerplate/imf
+rm -rf imf/${app_name}
+rm -rf imf/.git
+cd ..
+
+# Copy manifest.yml to /target
+cp manifest.yml target
+
